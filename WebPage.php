a8f5d7848e403b0b0f032d515e1649de58ac0c47
Gonzvp <43019932+Gonzvp@users.noreply.github.com>
2019-02-11 13:24:31 -0600

    first commit

---
+++ WebPage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head itemscope itemtype="http://schema.org/WebPage">
+
+  <meta name="description" content="">
+  <title></title>
+
+  <meta property="og:title" content="">
+  <meta property="og:url" content="">
+  <meta property="og:description" content="">
+  <meta property="og:image" content="">
+  //<meta property="og:type" content="">
+
+  <meta property="fb:app_id" content="">
+
+  <meta name="twitter:domain" content="">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="">
+  <meta name="twitter:url" content="">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:image" content="">
+
+  <script type="application/ld+json">
+    {
+      "@context":"http://schema.org",
+      "@type":"WebPage",
+      "name":"",
+      "alternateName":"",
+      "url:"",
+      "image":"",
+      "description":"",
+      "keywords":"",
+      "creator":"",
+      "material":"",
+    }
+  </script>
+
+</head>
+<body>
+
+ 
+
+</body>
+</html>
