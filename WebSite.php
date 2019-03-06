bee465511a208fc2d3e1b4bd108d87a1aab76d24
Gonzvp <Gonzvp@users.noreply.github.com>
2019-03-06 11:48:20 -0600

    second commit

---
+++ WebSite.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head itemscope itemtype="http://schema.org/WebSite">
+	
+	<meta name="description" content="">
+	<title></title>
+
+	<meta property="og:title" content="">
+	<meta property="og:url" content="">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="">
+	<meta property="og:type" content="">
+
+	<meta property="fb:app_id" content="">
+
+	<meta name="twitter:domain" content="">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="">
+	<meta name="twitter:url" content="">
+	<meta name="twitter:description" content="">
+	<meta name="twitter:image" content="">
+
+	<script type="application/ld+json">
+		{
+  			"@context": "http://schema.org",
+  			"@graph":
+  			[	
+  				{
+       				"@type": "WebSite",
+       				"name": "Servicios Legales de Propiedad Intelectual e Industrial SELPI - México",
+       				"alternateName":"SELPI",
+       				"url":"https://www.ipservices.com.mx/",
+       				"image":"https://www.ipservices.com.mx/images/optimized/selpi3_p.webp",
+       				"description":"Somos una firma de abogados especializada en derecho intelectual, rama del derecho internacional ocupada de la protección legal de marcas, avisos comerciales y diseños, obtencionde patentes, diseños industriales, modelos de utilidad, derechos de autor, secretos industriales, nombres de dominio, contratos de licencia y de franquicia.",
+       				"keywords":"registro, marca",
+       				"creator": 
+       					{
+       						"@type": "organization",
+       						"name":"Grupo Sincro",
+       						"url":"http://www.gruposincro.com/", 
+       						"sameAs": 
+       							[
+       								"http://www.facebook.com/"
+       								"http://www.instagram.com/"
+       							]
+       					}
+    			},
+    			{
+       				"@type": "LegalService",
+       				"name": "SELPI",
+       				"legalName":"Asesoria Legal en Propiedad Intelectual",
+       				"image":"https://www.ipservices.com.mx/images/optimized/selpi3_p.webp",
+       				"description":"Somos una firma de abogados especializada en derecho intelectual, rama del derecho internacional ocupada de la protección legal de marcas, avisos comerciales y diseños, obtencionde patentes, diseños industriales, modelos de utilidad, derechos de autor, secretos industriales, nombres de dominio, contratos de licencia y de franquicia.",
+      				"address":
+      					{
+        					"@type":"PostalAddress",
+            				"streetAddress":"calle",
+            				"addressLocality":"municipio",
+            				"addressRegion":"estado",
+            				"postalCode":"55010",
+            				"addressCountry":"MX"
+          				},
+      				"openingHours": 
+      					[
+    						"Mo-Fr 09:00-17:30",
+    						"Sa 09:00-12:00"
+  						]
+      				"currenciesAccepted": "MXN, USD",
+      				"paymentAccepted":"Efectivo, Tarjeta de Credito o Debito",
+      				"email": "info@ipservices.com.mx"
+    			}
+  			]
+		}
+	</script>
+
+</head>
+<body>
+
+</body>
+</html>
